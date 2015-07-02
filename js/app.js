28c174a308656268ddac695f02cdcc99a9f6d619
Christiane Ruetten <cr@mozilla.com>
2015-07-03 00:01:11 +0200

    Refactored keypad into object

--- js/app.js
+++ js/app.js
@@ -15,106 +15,118 @@ math.config({
     precision: 64
 });
 
-var buildKeyMatrix = function (div_id, width, height) {
-    var div = document.getElementById(div_id);
-    var table = document.createElement("table");
-    table.className = "keyboard";
-    table.style.width = "100%";
-    table.style.height = "100%";
-    var tbody = document.createElement("tbody");
+var Keypad = {
 
-    for (var y = 0; y < height; y++) {
-        var tr = document.createElement("tr");
-        for (var x = 0; x < width; x++) {
-            var td = document.createElement("td");
-            td.textContent = x + ":" + y;
-            td.style.width = "calc(100% / " + width + ")";
-            td.style.maxWidth = "calc(100% / " + width + ")";
-            td.style.height = "calc(100% / " + height + ")";
-            td.style.maxHeight = "calc(100% / " + height + ")";
-            td.addEventListener("click", pushButton);
-            tr.appendChild(td);
-            if (x == 0 && y == 1 && div_id == "number_panel")
-                td.rowSpan = 3;
-            if (x == 0 && y == 2 && div_id == "number_panel")
-                td.style.display = "none";
-            if (x == 0 && y == 3 && div_id == "number_panel")
-                td.style.display = "none";
+    init: function () {
+        this.buildMatrix("function_panel", 5, 4);
+        this.buildMatrix("number_panel", 5, 4);
+        for (var i = 0; i < this.keymap.length; i++) {
+            var k = this.keymap[i];
+            if (!k.inv) {
+                Keypad.findParentTd(k.pad, k.pos[0], k.pos[1]).textContent = k.label;
+            }
         }
-        tbody.appendChild(tr);
-    }
-    table.appendChild(tbody);
-    while (div.firstChild) {
-        div.removeChild(div.firstChild);
-    }
-    div.appendChild(table);
-};
+    },
 
-var setinv = function () {
+    buildMatrix: function (div_id, width, height) {
+        var div = document.getElementById(div_id);
+        var table = document.createElement("table");
+        table.className = "keyboard";
+        table.style.width = "100%";
+        table.style.height = "100%";
+        var tbody = document.createElement("tbody");
 
-};
+        for (var y = 0; y < height; y++) {
+            var tr = document.createElement("tr");
+            for (var x = 0; x < width; x++) {
+                var td = document.createElement("td");
+                td.textContent = x + ":" + y;
+                td.style.width = "calc(100% / " + width + ")";
+                td.style.maxWidth = "calc(100% / " + width + ")";
+                td.style.height = "calc(100% / " + height + ")";
+                td.style.maxHeight = "calc(100% / " + height + ")";
+                //td.addEventListener("click", pushButton);
+                tr.appendChild(td);
+                if (x == 0 && y == 1 && div_id == "number_panel")
+                    td.rowSpan = 3;
+                if (x == 0 && y == 2 && div_id == "number_panel")
+                    td.style.display = "none";
+                if (x == 0 && y == 3 && div_id == "number_panel")
+                    td.style.display = "none";
+            }
+            tbody.appendChild(tr);
+        }
+        table.appendChild(tbody);
+        while (div.firstChild) {
+            div.removeChild(div.firstChild);
+        }
+        div.appendChild(table);
+    },
 
-var setnorm = function () {
+    setinv: function () {
+    },
 
-};
+    setnorm: function () {
+    },
 
-var keymap = [
-    {label: "0", pad: "number_panel", pos: [1, 3], call: RPN.append},
-    {label: "1", pad: "number_panel", pos: [1, 2], call: RPN.append},
-    {label: "2", pad: "number_panel", pos: [2, 2], call: RPN.append},
-    {label: "3", pad: "number_panel", pos: [3, 2], call: RPN.append},
-    {label: "4", pad: "number_panel", pos: [1, 1], call: RPN.append},
-    {label: "5", pad: "number_panel", pos: [2, 1], call: RPN.append},
-    {label: "6", pad: "number_panel", pos: [3, 1], call: RPN.append},
-    {label: "7", pad: "number_panel", pos: [1, 0], call: RPN.append},
-    {label: "8", pad: "number_panel", pos: [2, 0], call: RPN.append},
-    {label: "9", pad: "number_panel", pos: [3, 0], call: RPN.append},
-    {label: "+", pad: "number_panel", pos: [4, 3], call: RPN.add, dump: true},
-    {label: "−", pad: "number_panel", pos: [4, 2], call: RPN.sub, dump: true},
-    {label: "×", pad: "number_panel", pos: [4, 1], call: RPN.mul, dump: true},
-    {label: "÷", pad: "number_panel", pos: [4, 0], call: RPN.div, dump: true},
-    {label: "E", pad: "number_panel", pos: [0, 0], call: RPN.append},
-    {label: "±", pad: "number_panel", pos: [3, 3], call: RPN.append},
-    {label: ".", pad: "number_panel", pos: [2, 3], call: RPN.append},
-    {label: "↵", pad: "number_panel", pos: [0, 1], call: RPN.enter, rowspan: 3}, // rowspan currently hardcoded
-    {label: "SIN", pad: "function_panel", pos: [0, 2], call: RPN.sin, dump: true},
-    {label: "ASIN", pad: "function_panel", pos: [0, 2], call: RPN.asin, dump: true, inv: true},
-    {label: "COS", pad: "function_panel", pos: [1, 2], call: RPN.cos, dump: true},
-    {label: "ACOS", pad: "function_panel", pos: [1, 2], call: RPN.acos, dump: true, inv: true},
-    {label: "TAN", pad: "function_panel", pos: [2, 2], call: RPN.tan, dump: true},
-    {label: "ATAN", pad: "function_panel", pos: [2, 2], call: RPN.atan, dump: true, inv: true},
-    {label: "√x", pad: "function_panel", pos: [3, 3], call: RPN.sqrt, dump: true},
-    {label: "x²", pad: "function_panel", pos: [3, 3], call: RPN.square, dump: true, inv: true},
-    {label: "1/x", pad: "function_panel", pos: [4, 3], call: RPN.recip, dump: true},
-    {label: "eˣ", pad: "function_panel", pos: [0, 3], call: RPN.exp, dump: true},
-    {label: "ln", pad: "function_panel", pos: [0, 3], call: RPN.ln, dump: true, inv: true},
-    {label: "10ˣ", pad: "function_panel", pos: [1, 3], call: RPN.exp10, dump: true},
-    {label: "log", pad: "function_panel", pos: [1, 3], call: RPN.log, dump: true, inv: true},
-    {label: 'yˣ', pad: "function_panel", pos: [2, 3], call: RPN.pow, dump: true},
-    {label: 'ˣ√y', pad: "function_panel", pos: [2, 3], call: RPN.xsqrt, dump: true, inv: true},
-    {label: "R↓", pad: "function_panel", pos: [1, 0], call: RPN.rotdn, dump: true},
-    {label: "R↑", pad: "function_panel", pos: [1, 0], call: RPN.rotup, dump: true, inv: true},
-    {label: 'C', pad: "function_panel", pos: [3, 0], call: RPN.clr},
+    keymap: [
+        {label: "0", pad: "number_panel", pos: [1, 3], call: RPN.append},
+        {label: "1", pad: "number_panel", pos: [1, 2], call: RPN.append},
+        {label: "2", pad: "number_panel", pos: [2, 2], call: RPN.append},
+        {label: "3", pad: "number_panel", pos: [3, 2], call: RPN.append},
+        {label: "4", pad: "number_panel", pos: [1, 1], call: RPN.append},
+        {label: "5", pad: "number_panel", pos: [2, 1], call: RPN.append},
+        {label: "6", pad: "number_panel", pos: [3, 1], call: RPN.append},
+        {label: "7", pad: "number_panel", pos: [1, 0], call: RPN.append},
+        {label: "8", pad: "number_panel", pos: [2, 0], call: RPN.append},
+        {label: "9", pad: "number_panel", pos: [3, 0], call: RPN.append},
+        {label: "+", pad: "number_panel", pos: [4, 3], call: RPN.add, dump: true},
+        {label: "−", pad: "number_panel", pos: [4, 2], call: RPN.sub, dump: true},
+        {label: "×", pad: "number_panel", pos: [4, 1], call: RPN.mul, dump: true},
+        {label: "÷", pad: "number_panel", pos: [4, 0], call: RPN.div, dump: true},
+        {label: "E", pad: "number_panel", pos: [0, 0], call: RPN.append},
+        {label: "±", pad: "number_panel", pos: [3, 3], call: RPN.append},
+        {label: ".", pad: "number_panel", pos: [2, 3], call: RPN.append},
+        {label: "↵", pad: "number_panel", pos: [0, 1], call: RPN.enter, rowspan: 3}, // rowspan currently hardcoded
+        {label: "SIN", pad: "function_panel", pos: [0, 2], call: RPN.sin, dump: true},
+        {label: "ASIN", pad: "function_panel", pos: [0, 2], call: RPN.asin, dump: true, inv: true},
+        {label: "COS", pad: "function_panel", pos: [1, 2], call: RPN.cos, dump: true},
+        {label: "ACOS", pad: "function_panel", pos: [1, 2], call: RPN.acos, dump: true, inv: true},
+        {label: "TAN", pad: "function_panel", pos: [2, 2], call: RPN.tan, dump: true},
+        {label: "ATAN", pad: "function_panel", pos: [2, 2], call: RPN.atan, dump: true, inv: true},
+        {label: "√x", pad: "function_panel", pos: [3, 3], call: RPN.sqrt, dump: true},
+        {label: "x²", pad: "function_panel", pos: [3, 3], call: RPN.square, dump: true, inv: true},
+        {label: "1/x", pad: "function_panel", pos: [4, 3], call: RPN.recip, dump: true},
+        {label: "eˣ", pad: "function_panel", pos: [0, 3], call: RPN.exp, dump: true},
+        {label: "ln", pad: "function_panel", pos: [0, 3], call: RPN.ln, dump: true, inv: true},
+        {label: "10ˣ", pad: "function_panel", pos: [1, 3], call: RPN.exp10, dump: true},
+        {label: "log", pad: "function_panel", pos: [1, 3], call: RPN.log, dump: true, inv: true},
+        {label: 'yˣ', pad: "function_panel", pos: [2, 3], call: RPN.pow, dump: true},
+        {label: 'ˣ√y', pad: "function_panel", pos: [2, 3], call: RPN.xsqrt, dump: true, inv: true},
+        {label: "R↓", pad: "function_panel", pos: [1, 0], call: RPN.rotdn, dump: true},
+        {label: "R↑", pad: "function_panel", pos: [1, 0], call: RPN.rotup, dump: true, inv: true},
+        {label: 'C', pad: "function_panel", pos: [3, 0], call: RPN.clr},
 //      {label: "CLx", pad: "function_panel", pos:[3,0], call:RPN.clx, inv: true}, //TODO
-    {label: "⌫", pad: "function_panel", pos: [4, 0], call: RPN.del},
+        {label: "⌫", pad: "function_panel", pos: [4, 0], call: RPN.del},
 //      {label: "UNDO", pad: "function_panel", pos:[4,0], call:RPN.undo, inv: true}, //TODO
-    {label: "×π", pad: "function_panel", pos: [3, 2], call: RPN.timespi, dump: true},
+        {label: "×π", pad: "function_panel", pos: [3, 2], call: RPN.timespi, dump: true},
 //      {label: "e", pad: "function_panel", pos:[3,2], call:RPN.pi, dump:true, inv: true}, // TODO
-    {label: "x↔︎y", pad: "function_panel", pos: [2, 0], call: RPN.swap, dump: true},
-    {label: "RAD", pad: "function_panel", pos: [4, 2], call: RPN.rad, dump: true},
-    {label: "DEG", pad: "function_panel", pos: [4, 2], call: RPN.deg, dump: true, inv: true},
-    {label: "INV", pad: "function_panel", pos: [0, 0], call: setinv},
-    {label: "INV", pad: "function_panel", pos: [0, 0], call: setnorm, inv: true}
-];
+        {label: "x↔︎y", pad: "function_panel", pos: [2, 0], call: RPN.swap, dump: true},
+        {label: "RAD", pad: "function_panel", pos: [4, 2], call: RPN.rad, dump: true},
+        {label: "DEG", pad: "function_panel", pos: [4, 2], call: RPN.deg, dump: true, inv: true},
+        {label: "INV", pad: "function_panel", pos: [0, 0], call: this.setinv},
+        {label: "INV", pad: "function_panel", pos: [0, 0], call: this.setnorm, inv: true}
+    ],
 
-var findParentTd = function (keypad, x, y) {
-    var tbody = document.getElementById(keypad).firstChild.firstChild;
-    return tbody.children[y].children[x];
-};
+    findParentTd: function (keypad, x, y) {
+        var tbody = document.getElementById(keypad).firstChild.firstChild;
+        return tbody.children[y].children[x];
+    },
 
-var findKey = function (label) {
-    for (var i = 0; i < keymap.length; i++) {
-        if (keymap[i].label == label) return keymap[i];
+    findKey: function (label) {
+        for (var i = 0; i < this.keymap.length; i++) {
+            if (this.keymap[i].label == label) return this.keymap[i];
+        }
     }
 };
 
@@ -141,27 +153,25 @@ var pushButton = function (event) {
     if (td) {
         navigator.vibrate([30]);
         //console.log(td);
-        var key = findKey(td.textContent);
+        var key = Keypad.findKey(td.textContent);
         console.log("calling", key.call, "with arg", key.label);
         if (key.dump) RPN.dump.bind(RPN)();
         if (key.call) key.call.bind(RPN, key.label)();
-        console.log("result: ", RPN.stack, RPN.buffer);
+        console.log("result: ", RPN.stack.stack, RPN.buffer);
         updateDisplay();
     }
 };
 
+var multitouchHandler = function (event) {
+    //console.log(event);
+    pushButton(event);
+}
+
 var init = function () {
     console.log("READY.");
-    buildKeyMatrix("function_panel", 5, 4);
-    buildKeyMatrix("number_panel", 5, 4);
-    for (var i = 0; i < keymap.length; i++) {
-        var k = keymap[i];
-        if (!k.inv) {
-            findParentTd(k.pad, k.pos[0], k.pos[1]).textContent = k.label;
-        }
-    }
-    //document.addEventListener("ontouchstart", pushButton);
+    Keypad.init();
     updateDisplay();
+    document.addEventListener("touchstart", function (event) {multitouchHandler(event)});
 };
 
 window.addEventListener('load', function load(event) {
